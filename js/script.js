50cdc769c6bc602b7a764808ce848e90ba15fca4
Andrey Yashchanka <And075555@gmail.com>
2023-02-17 21:24:34 +0300

    feat: add lesson 9 Operators

--- js/script.js
+++ js/script.js
@@ -1,11 +1,92 @@
 "use strict";
 
-const category = 'toys';
+console.log('arr' + " - object");
+console.log(4 + +"5");
 
-// console.log('https://someurl.com/' + category + '/' + '4');
+// Операторы инкремента
 
-console.log(`https://someurl.com/${category}/5`);
+let incr = 10,
+    decr = 10;
 
-const user = "Andrey";
+incr++;
+// оператор инкремента постфиксный
+decr--;
+// оператор декремента
 
-alert(`Привет, ${user}`);
+++incr;
+// оператор инкремента префиксный
+--decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5 % 2);
+// 5 разделили на 2 столько раз, сколько это возможно и в результате получили остаток 1
+
+
+console.log(2 * 4 == 8);
+// Результат: True
+console.log(2 * 4 == '8');
+// Результат: True
+console.log(2 * 4 === '8');
+// Результат: false
+console.log(2 * 4 === 8);
+// Результат: true
+//  == знак сравнения
+//  === знак сравнения (число строго должно равняться числу)
+
+
+
+//  &&    оператор "И" записывается как 2 амперсанта
+//  ||    оператор "ИЛИ" записывается как 2 вертикальные черты
+
+const isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
+// в результате получим    true
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && isClose);
+// в результате получим    false
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked || isClose);
+// в результате получим    true
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked || isClose);
+// в результате получим    false
+
+//  !    оператор отрицания
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked || !isClose);
+// в результате получим    true
+
+
+// ПОРЯДОК ВЫПОЛНЕНИЯ ОПЕРАТОРОВ
+
+console.log(2 + 2 * 2 == 8);
+// Результат: False
+
+console.log(2 + 2 * 2 != 8);
+// Результат: True
+
+console.log(2 + 2 * 2 != '6');
+// Результат: False
+
+console.log(2 + 2 * 2 !== '6');
+// Результат: True
