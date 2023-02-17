16ffe748268f5291787786c37dc4d759e5ae10ba
Andrey Yashchanka <And075555@gmail.com>
2023-02-17 19:19:00 +0300

    feat: add lesson 7 simple communication with the user

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 "use strict";
 
-const obj = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};
+// alert('Hello');
 
-// console.log(obj.name);
-console.log(obj["name"]);
+// const result = confirm("Are you here?");
+// console.log(result); 
 
-let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-console.log(arr[1]);
+// const answer = +prompt("Dfv tcnm 18?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваша фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+document.write(answers);
+
+// console.log(typeof(answers));
